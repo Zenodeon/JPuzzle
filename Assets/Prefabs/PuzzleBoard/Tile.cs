a6ab232345c0498cb85df8647380e7c82a38b3ef
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-24 16:20:23 +0530

    Tile Direction Stuff (Not Done)

--- Assets/Prefabs/PuzzleBoard/Tile.cs
+++ Assets/Prefabs/PuzzleBoard/Tile.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 using TMPro;
 
 using NaughtyAttributes;
@@ -9,13 +11,16 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] public RectTransform rectTransform;
     [SerializeField] public TextMeshProUGUI tmp;
-
+    [Space]
     [ReadOnly] public Vector2 ID;
     [ReadOnly] public Vector2 coords;
+    [ReadOnly] public Vector2 previousCoords;
 
     private Vector2 size;
     private Vector2 spacing;
     private Vector2 offset;
+
+    public UnityEvent<Tile, Vector2> OnMoved = new UnityEvent<Tile, Vector2>();
 
     public void Setup(Vector2 ID, Vector2 size, Vector2 spacing, Vector2 offset)
     {
@@ -43,8 +48,31 @@ public class Tile : MonoBehaviour
 
     public void Move(Vector2 dir)
     {
-        Debug.Log(ID + " : " + dir);
+        previousCoords = coords;
         coords += dir;
+
         UpdateTransform();
+
+        OnMoved.Invoke(this, previousCoords);
+    }
+
+    public void ShowDirection(BoardInput.InputDir dir)
+    {
+        switch (dir)
+        {
+            case BoardInput.InputDir.Up:
+                break;
+
+            case BoardInput.InputDir.Down:
+                break;
+
+            case BoardInput.InputDir.Left:
+                break;
+
+            case BoardInput.InputDir.Right:
+                break;
+
+            default: break;
+        }
     }
 }
