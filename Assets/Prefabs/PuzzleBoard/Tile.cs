4f225ac1827e18a8b191d7fed5f285553f848887
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-25 16:03:33 +0530

    Shows Moveable Direction and can self move

--- Assets/Prefabs/PuzzleBoard/Tile.cs
+++ Assets/Prefabs/PuzzleBoard/Tile.cs
@@ -13,21 +13,29 @@ public class Tile : MonoBehaviour
     [SerializeField] public RectTransform rectTransform;
     [SerializeField] public TextMeshProUGUI tmp;
     [Space]
-    [SerializeField] public AnimationCurve moveCurve;
-    [Space]
     [ReadOnly] public int indexID;
     [ReadOnly] public Vector2 ID;
     [ReadOnly] public Vector2 coords;
     [ReadOnly] public Vector2 previousCoords;
+    [Space]
+    [SerializeField] private Mask lm;
+    [SerializeField] private Mask tm;
+    [SerializeField] private Mask rm;
+    [SerializeField] private Mask bm;
+
+    private PuzzleBoard board;
 
     private Vector2 size;
     private Vector2 spacing;
     private Vector2 offset;
 
+    public Vector2 moveableDir = Vector2.zero;
+
     public UnityEvent<Tile, Vector2, bool> OnMoved = new UnityEvent<Tile, Vector2, bool>();
 
-    public void Setup(int indexID, Vector2 ID, Vector2 size, Vector2 spacing, Vector2 offset)
+    public void Setup(PuzzleBoard board, int indexID, Vector2 ID, Vector2 size, Vector2 spacing, Vector2 offset)
     {
+        this.board = board;
         this.indexID = indexID;
         this.ID = ID;
         tmp.text = indexID + "";
@@ -52,34 +60,39 @@ public class Tile : MonoBehaviour
         rectTransform.anchoredPosition = (coords * (size + spacing)) + offset;
     }
 
-    public void Move(Vector2 dir, bool setupMode = false)
+    public void Move(AnimationCurve movementCurve, bool setupMode = false)
     {
         previousCoords = coords;
 
-        Vector2 newCoord = coords + dir;
-        DOTween.To(() => coords, x => coords = x, newCoord, setupMode ? 0.05f : 2)
-            .SetEase(moveCurve)
+        Vector2 newCoord = coords + moveableDir;
+        DOTween.To(() => coords, x => coords = x, newCoord, setupMode ? 0.04f : 0.1f)
+            .SetEase(movementCurve)
             .OnUpdate(UpdateTransform)
             .OnComplete(() => OnMoved.Invoke(this, previousCoords, setupMode));
     }
 
-    public void ShowDirection(BoardInput.InputDir dir)
+    public void SelfMove()
     {
-        switch (dir)
-        {
-            case BoardInput.InputDir.Up:
-                break;
+        board.TryMoveTile(this);   
+    }
 
-            case BoardInput.InputDir.Down:
-                break;
+    public void ShowDirection()
+    {
+        lm.showMaskGraphic = false;
+        tm.showMaskGraphic = false;
+        rm.showMaskGraphic = false;
+        bm.showMaskGraphic = false;
 
-            case BoardInput.InputDir.Left:
-                break;
+        if(moveableDir == Vector2.left)
+            lm.showMaskGraphic = true;
 
-            case BoardInput.InputDir.Right:
-                break;
+        if (moveableDir == Vector2.up)
+            tm.showMaskGraphic = true;
 
-            default: break;
-        }
+        if(moveableDir == Vector2.right)
+            rm.showMaskGraphic = true;
+
+        if (moveableDir == Vector2.down)
+            bm.showMaskGraphic = true;
     }
 }
