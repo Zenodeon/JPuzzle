d3ee4a3ad1de436c6b5ee46debc1178a62b5e9b4
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-07-26 12:24:19 +0530

    BoardData

---
+++ Assets/Scripts/BoardData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoardData
+{
+    public UnityEvent OnDataUpdated = new UnityEvent();
+
+    public Vector2 gridSize;
+
+    public Vector2 tileSize;
+    public Vector2 tileSpacing;
+    public float borderTilePercent;
+    public Vector2 tileOffset => (tileSize * borderTilePercent) * new Vector2(1, -1);
+
+    public int moves;
+
+    public static readonly List<Vector2> dirTable = new List<Vector2>()
+    {
+        Vector2.left,
+        Vector2.up,
+        Vector2.right,
+        Vector2.down
+    };
+}
